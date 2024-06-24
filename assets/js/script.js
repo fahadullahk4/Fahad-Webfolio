89a0a56109e3270ba99e11da0e3da4ba28e8588e
Fahad Ullah Khan <fahadullahk4@gmail.com>
2024-06-24 19:19:59 +0500

    completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,28 +1,22 @@
 "use strict";
 
-// element toggle function
 const elementToggleFunc = function (elem) {
 	elem.classList.toggle("active");
 };
 
-// sidebar variables
 const sidebar = document.querySelector("[data-sidebar]");
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
-// sidebar toggle functionality for mobile
 sidebarBtn.addEventListener("click", function () {
 	elementToggleFunc(sidebar);
 });
 
-// contact form variables
 const form = document.querySelector("[data-form]");
 const formInputs = document.querySelectorAll("[data-form-input]");
 const formBtn = document.querySelector("[data-form-btn]");
 
-// add event to all form input field
 for (let i = 0; i < formInputs.length; i++) {
 	formInputs[i].addEventListener("input", function () {
-		// check form validation
 		if (form.checkValidity()) {
 			formBtn.removeAttribute("disabled");
 		} else {
@@ -31,11 +25,9 @@ for (let i = 0; i < formInputs.length; i++) {
 	});
 }
 
-// page navigation variables
 const navigationLinks = document.querySelectorAll("[data-nav-link]");
 const pages = document.querySelectorAll("[data-page]");
 
-// add event to all nav link
 for (let i = 0; i < navigationLinks.length; i++) {
 	navigationLinks[i].addEventListener("click", function () {
 		for (let i = 0; i < pages.length; i++) {
